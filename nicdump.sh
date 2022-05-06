a6854e5038d2597b7d9801085068f62a9d2abd00
Calvin Judy <calvin@swiftnode.net>
2022-05-06 05:38:54 -0400

    Update nicdump.sh

--- nicdump.sh
+++ nicdump.sh
@@ -5,4 +5,4 @@ echo "/ __\\ \\ /\\ / / | |_| '_ \\ / _ \ / _\` |/ _ \\ | '_ \\ / _ \\ __|"
 echo "\\__ \\\\ V  V /| |  _| | | | (_) | (_| |  __/_| | | |  __/ |_"
 echo "|___/ \\_/\\_/ |_|_| |_| |_|\\___/ \\__,_|\\___(_)_| |_|\\___|\\__|"
 INTERFACE="$(cat ./settings.ini | grep 'interface' | awk '{print $3}' | tr -d '\011\012\013\014\015\040\042' | tr -d ';')"
-tcpdump -l -e -n -v -i ${INTERFACE} | ./netbps.pl
+tcpdump -B 4096 -e -n -v -i ${INTERFACE} | ./netbps.pl
